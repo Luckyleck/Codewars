54317c0480b5b97fb8a127837e76bb6c683de922
Alex L <luckyleck29@gmail.com>
2023-12-05 00:26:04 -0500

    Adding SumMix

---
+++ SumMix/solution.js
@@ -0,0 +1,7 @@
+function sumMix(nums) {
+    return nums.reduce((a, b) => Number(a) + Number(b), 0)
+}
+
+console.log(sumMix([9, 3, '7', '3']))
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']))
